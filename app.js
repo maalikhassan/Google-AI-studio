24355ed14aa2d79e461c544ea0d3b29a49ddacaf
Maalik_Hassan <maalikhassan1@gmail.com>
2025-11-25 12:45:29 +0530

    set up strcuture and converted postman function to js

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
-const myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-myHeaders.append("X-goog-api-key", "AIzaSyDUftPaIxInykUGoLD7mLWQgHbBoeSi9Eo");
+function search() {
 
-const raw = JSON.stringify({
-  "contents": [
-    {
-      "parts": [
-        {
-          "text": "Explain how AI works in a few words"
-        }
-      ]
-    }
-  ]
-});
 
-const requestOptions = {
-  method: "POST",
-  headers: myHeaders,
-  body: raw,
-  redirect: "follow"
-};
+    let txtPrompt = document.getElementById("txtPrompt").value;
 
-fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent", requestOptions)
-  .then((response) => response.json())
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("X-goog-api-key", "AIzaSyDUftPaIxInykUGoLD7mLWQgHbBoeSi9Eo");
+
+  const raw = JSON.stringify({
+    contents: [
+      {
+        parts: [
+          {
+            text: txtPrompt,
+          },
+        ],
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
+}
